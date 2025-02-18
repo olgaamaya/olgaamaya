7889e9efe4f11fae8db5d4fa801778d108a7861a
olgaamaya <olgaamaya28@gmail.com>
2025-02-18 18:16:29 +0100

    change

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,10 @@
 function scale(x) {
-  if (x.matches) { // If media query matches
-    document.getElementById('menu-toggle').style.display = 'flex';
-  } else {
-    document.getElementById('menu-toggle').style.display = 'none';
-  }}
+    if (x.matches) { // If media query matches
+        document.getElementById('menu-toggle').style.display = 'flex';
+    } else {
+        document.getElementById('menu-toggle').style.display = 'none';
+    }
+}
 var x = window.matchMedia("(max-width: 950px)")
 scale(x) // Call listener function at run time
 x.addListener(scale) // Attach listener function on state changes 
@@ -20,18 +21,18 @@ const morecontact = document.getElementById("more");
 const lesscontact = document.getElementById("less");
 
 menu.addEventListener('click', () => {
-      navMenu.classList.toggle('show');
-      navUL.classList.toggle('show');
-      address.classList.toggle('show');
-      body.classList.toggle('show');
-      menu.classList.toggle('show');
-      menupoint.classList.toggle('show');
-      h_index.classList.toggle('h_index');
-      contact.classList.toggle('contact');
-      morecontact.classList.toggle('morecontact');
-      lesscontact.classList.toggle('lesscontact');
+    navMenu.classList.toggle('show');
+    navUL.classList.toggle('show');
+    address.classList.toggle('show');
+    body.classList.toggle('show');
+    menu.classList.toggle('show');
+    menupoint.classList.toggle('show');
+    h_index.classList.toggle('h_index');
+    contact.classList.toggle('contact');
+    morecontact.classList.toggle('morecontact');
+    lesscontact.classList.toggle('lesscontact');
 
-}); 
+});
 
 
 
@@ -39,15 +40,17 @@ menu.addEventListener('click', () => {
 //TO TOP BUTTON
 mybutton = document.getElementById("myBtn");
 // [When the user scrolls down 20px from the top of the document, show the button]
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
+
 function scrollFunction() {
-     if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
-       mybutton.style.display = "flex";} 
-       else {
-       mybutton.style.display = "none";}
-   };
+    if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+        mybutton.style.display = "flex";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
 // [When the user clicks on the button, scroll to the top of the document]
 function topFunction() {
- document.body.scrollTop = 0; // For Safari
- document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
