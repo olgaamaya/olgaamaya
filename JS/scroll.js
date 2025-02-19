22f85eb5a8f07dfc5438743eb998f524f92b17f4
olgaamaya <olgaamaya28@gmail.com>
2025-02-19 12:02:53 +0100

    change

---
+++ JS/scroll.js
@@ -0,0 +1,38 @@
+function scrollpage() {
+    function f() {
+        window.scrollTo(0, i);
+        if (status == 0) {
+            i = i + 1.75;
+            if (i >= Height) { status = 1; }
+        }
+
+        //else {
+        //  i = i - 4;
+        //    if (i <= 1) { status = 0; } // if you don't want continue scroll then remove this line
+        // }
+        setTimeout(f, 0.01);
+    }
+    f();
+}
+var Height = document.documentElement.scrollHeight;
+var i = 1,
+    j = Height,
+    status = 0;
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        scrollpage();
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
+    }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(min-width: 1500px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+    myFunction(x);
+});
