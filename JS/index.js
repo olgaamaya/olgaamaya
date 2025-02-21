e8d7651e07ad9ff0ad35a723f7f7439fdb5cfd72
olgaamaya <olgaamaya28@gmail.com>
2025-02-21 13:59:43 +0100

    randomcolor

--- JS/index.js
+++ JS/index.js
@@ -1,65 +1,75 @@
-var sizer = window.matchMedia( "(min-width: 950px)" );
+function generateRandomColor() {
+    var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    if (randomColor.length != 7) { // In any case, the color code is invalid
+        randomColor = generateRandomColor();
+    }
+    return randomColor;
+    // The random color will be freshly served
+}
+document.body.style.backgroundColor = generateRandomColor() // -> #E1AC94
+document.documentElement.style.setProperty('--main-bg-color', randomColor);
+
+
+var sizer = window.matchMedia("(min-width: 950px)");
 var mouseTarget = document.getElementById('mouseTarget');
 // var mouseTarget2 = document.getElementById('social_elements'); //
 
 const collection = document.getElementsByClassName("figure.project-index");
 let numb = document.getElementsByClassName("figure.project-index").length;
 
-mouseTarget.addEventListener('mouseenter', function(){ 
- if (sizer.matches) {
- document.getElementById("bodyid").style.background = "var(--secondcolor)";
- for (let i = 0; i < collection.length; i++) {
-  collection[i].style.border = "solid 2px pink";
-}
-}
-else {
- document.getElementById("bodyid").style.background = '';
- for (let i = 0; i < collection.length; i++) {
-  collection[i].style.border = "solid 2px pink";
-}
-}
-}
-);
+mouseTarget.addEventListener('mouseenter', function() {
+    if (sizer.matches) {
+        document.getElementById("bodyid").style.background = "var(--secondcolor)";
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.border = "solid 2px pink";
+        }
+    } else {
+        document.getElementById("bodyid").style.background = '';
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.border = "solid 2px pink";
+        }
+    }
+});
 
-mouseTarget.addEventListener('mouseleave', function(){
- if (sizer.matches) {
- document.getElementById("bodyid").style.background = "";
-  for (let i = 0; i < collection.length; i++) {
-  collection[i].style.border = "solid 2px pink";
-}
-}
-else {
- document.getElementById("bodyid").style.background = "";
- for (let i = 0; i < collection.length; i++) {
-  collection[i].style.border = "solid 2px pink";
-}}});
+mouseTarget.addEventListener('mouseleave', function() {
+    if (sizer.matches) {
+        document.getElementById("bodyid").style.background = "";
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.border = "solid 2px pink";
+        }
+    } else {
+        document.getElementById("bodyid").style.background = "";
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.border = "solid 2px pink";
+        }
+    }
+});
 
 
-mouseTarget2.addEventListener('mouseenter', function(){ 
- if (sizer.matches) {
- document.getElementById("bodyid").style.background = "var(--secondcolor)";
- for (let i = 0; i < collection.length; i++) {
-  collection[i].style.border = "solid 2px pink";
-}
-}
-else {
- document.getElementById("bodyid").style.background = '';
- for (let i = 0; i < collection.length; i++) {
-  collection[i].style.border = "solid 2px pink";
-}
-}
-}
-);
+mouseTarget2.addEventListener('mouseenter', function() {
+    if (sizer.matches) {
+        document.getElementById("bodyid").style.background = "var(--secondcolor)";
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.border = "solid 2px pink";
+        }
+    } else {
+        document.getElementById("bodyid").style.background = '';
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.border = "solid 2px pink";
+        }
+    }
+});
 
-mouseTarget2.addEventListener('mouseleave', function(){
- if (sizer.matches) {
- document.getElementById("bodyid").style.background = "";
-  for (let i = 0; i < collection.length; i++) {
-  collection[i].style.border = "solid 2px pink";
-}
-}
-else {
- document.getElementById("bodyid").style.background = "";
- for (let i = 0; i < collection.length; i++) {
-  collection[i].style.border = "solid 2px pink";
-}}});
+mouseTarget2.addEventListener('mouseleave', function() {
+    if (sizer.matches) {
+        document.getElementById("bodyid").style.background = "";
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.border = "solid 2px pink";
+        }
+    } else {
+        document.getElementById("bodyid").style.background = "";
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.border = "solid 2px pink";
+        }
+    }
+});
