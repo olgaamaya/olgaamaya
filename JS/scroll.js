be834da8fc185c2b22aa8a388cfbfd22d108d1c3
olgaamaya <olgaamaya28@gmail.com>
2025-02-24 14:53:19 +0100

    Update scroll.js

--- JS/scroll.js
+++ JS/scroll.js
@@ -2,6 +2,7 @@ var meinIntervall = setInterval(function() {
     document.getElementById("headline2").innerHTML = "Let's get in contact!";
 }, 0); // 0 für timing direkt
 
+// Define the scrollpage function to handle the scrolling logic
 function scrollpage() {
     function f() {
         window.scrollTo(0, i);
@@ -10,22 +11,28 @@ function scrollpage() {
             if (i >= Height) { status = 1; }
         }
 
-        //else {
-        //  i = i - 4;
-        //    if (i <= 1) { status = 0; } // if you don't want continue scroll then remove this line
-        // }
+        // Set a small delay to create the effect of continuous scrolling
         setTimeout(f, 0.01);
     }
     setTimeout(f, 1000);
 }
+
 var Height = document.documentElement.scrollHeight;
 var i = 1,
     j = Height,
     status = 0;
 
+// Function that applies styles when a media query condition is met
 function myFunction(x) {
-    if (x.matches) { // If media query matches
+    if (x.matches) { // If the window width is at least 1250px
+
+        // Start the custom scrolling
         scrollpage();
+
+        // Show headline and headline2, adjust their styles
+        document.getElementById("headline").style.display = "block";
+        document.getElementById("headline2").style.display = "block";
+
         document.getElementsByTagName("body")[0].style.overflow = "hidden";
         document.getElementById("headline").style.margin = "10vh 0 0 0";
         document.getElementById("headline2").style.height = "5vh";
@@ -33,17 +40,21 @@ function myFunction(x) {
         document.getElementById("social_elements").style.height = "20vh";
         document.getElementsByTagName("footer")[0].style.height = "20vh";
         document.getElementById("wrap_info_id").style.height = "55vh";
+    } else { // If the window width is less than 1250px
 
+        // Hide headline and headline2 for small screens
+        document.getElementById("headline").style.display = "none";
+        document.getElementById("headline2").style.display = "none";
     }
 }
 
-// Create a MediaQueryList object
-var x = window.matchMedia("(min-width: 1250px)")
+// Create a MediaQueryList object to track changes in the viewport width (min-width: 1250px)
+var x = window.matchMedia("(min-width: 1250px)");
 
-// Call listener function at run time
+// Call the `myFunction` immediately to check if the media query matches when the page loads
 myFunction(x);
 
-// Attach listener function on state changes
+// Add an event listener to the MediaQueryList object to listen for changes in the viewport width
 x.addEventListener("change", function() {
     myFunction(x);
 });
