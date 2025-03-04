ee5578cbd06f5f4c019c06452103dcf3aac3b546
olgaamaya <olgaamaya28@gmail.com>
2025-03-04 23:18:50 +0100

    new web with array
    array

--- JS/landing.js
+++
@@ -1,6 +0,0 @@
-function change_text_on(){
-    document.getElementById("portfolio").innerHTML = "Portfolio";
-}
-function change_text_of(){
-    document.getElementById("portfolio").innerHTML = "Olga Amaya";
-}
